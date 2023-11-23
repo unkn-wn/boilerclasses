98cfc5533c6705bd5ef918fd8c0e7e25eccc7a56
sarthak <sarthak.robo@gmail.com>
2023-11-23 14:52:29 -0800

    update db stuff

--- python/db.py
+++ python/db.py
@@ -3,22 +3,25 @@ import json
 import redis
 from redis.commands.json.path import Path
 from tqdm import tqdm
+from dotenv import load_dotenv
 
+load_dotenv()
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
+r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, username="default", password=REDIS_PASSWORD)
 
 all_classes = []
 
 count = 1
-for jsonFile in os.listdir("classes/"):
-  path = "classes/" + jsonFile
-  f = open(path)
-  data = json.load(f)
-  all_classes.extend(data)
-  # for classData in tqdm(data):
-  #   key = "Class:" + str(count)
-  #   classData['courseCode'] = int(classData['courseCode'])
-  #   r.json().set(key, Path.root_path(), classData)
-  #   count += 1
-  print("done", jsonFile)
+path = "out.json"
+f = open(path)
+data = json.load(f)
+for classData in tqdm(data):
+  key = "classes:" + str(count)
+  r.json().set(key, Path.root_path(), classData)
+  count += 1
 
-outfile = open("all.json", "w")
-json.dump(all_classes, outfile, indent=4)
+print("finished!")
