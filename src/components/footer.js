3e0722a3e69822fa5d11b0bfe58ea2d2f37ae751
Leon <leon.yee000@gmail.com>
2023-12-03 22:38:24 -0500

    footer update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
+import {
+    Image
+} from '@chakra-ui/react'
+
 const Footer = () => {
-  return (
-    <div className='grid justify-center py-5'>
-      <p className='text-gray-700 text-sm text-center'>
-        BoilerClasses is an unofficial catalogue for Purdue courses made by Purdue students. <br />
-        We'd love to hear your <a href="https://forms.gle/jtpLPbXm4X4RFoNh6" target="_black" rel="noopener noreferrer" className='text-gray-500'>feedback! </a>
-        Inspired by <a href='https://classes.wtf/' target='_blank' rel='noopener noreferrer' className='text-gray-500'>classes.wtf</a><br />
-      </p>
-    </div>
-  );
+    return (
+        <div className='grid justify-center py-5'>
+            <p className='text-gray-700 text-sm text-center break-words'>
+                <div className='flex items-center justify-center'>
+                    <a href='https://github.com/unkn-wn/boilerclasses' target="_black" rel="noopener noreferrer" className='text-gray-500 flex items-center gap-1'>
+                        <Image src="https://icones.pro/wp-content/uploads/2021/06/icone-github-grise.png" alt="" boxSize={4} />
+                        BoilerClasses
+                    </a>
+                </div>
+                is an unofficial catalogue for Purdue courses made by Purdue students. <br />
+                We'd love to hear your <a href="https://forms.gle/jtpLPbXm4X4RFoNh6" target="_black" rel="noopener noreferrer" className='text-gray-500'>feedback! </a>
+                Inspired by <a href='https://classes.wtf/' target='_blank' rel='noopener noreferrer' className='text-gray-500'>classes.wtf</a><br />
+            </p>
+        </div>
+    );
 };
 
 export default Footer;
