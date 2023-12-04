cd53dcac154998312535293e71dce524f029942f
Leon <leon.yee000@gmail.com>
2023-12-03 22:45:42 -0500

    Vercel analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import { CacheProvider } from '@chakra-ui/next-js';
 import {  MultiSelectTheme } from 'chakra-multiselect'
+import { Analytics } from '@vercel/analytics/react';
 import Head from "next/head";
 
 const theme = extendTheme({
@@ -14,6 +15,7 @@ const theme = extendTheme({
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Analytics />
       <Head>
         <title>BoilerClasses</title>
       </Head>
