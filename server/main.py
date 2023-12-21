23e1043efae2a7e3ffe63a5f87dd76f1560a3b1a
sarthak <sarthak.robo@gmail.com>
2023-12-21 17:10:20 +0530

    not functional but progress

--- server/main.py
+++ server/main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import redis
 app = Flask(__name__)
 
 @app.route('/query', methods = ["GET", "POST"])
 def query():
-  
-  return "hello"
+  if request.method == 'GET':
+    q = request.args.get('q')
+    r = redis.Redis(host='localhost', port=7501)
+    res = r.ft("idx:classes").search(q).docs
+    full_res = []
+    for result in list(res):
+      full_res.append(result.json)
+    return full_res
 
 if __name__ == "__main__":
   app.run(host= "0.0.0.0", debug=True)
--- src/pages/api/search.js
+++ src/pages/api/search.js
@@ -6,12 +6,14 @@ import { searchCourses } from "../../lib/redis"
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     const q = req.query.q.trim();
-    const subjects = req.query.sub;
-    const terms = req.query.term;
-    const gen = req.query.gen;
-    const cmin = req.query.cmin;
-    const cmax = req.query.cmax;
-    const courses = await searchCourses(q, subjects.split(","), terms.split(","), gen.split(","), cmin, cmax);
+    const subjects = req.query.sub || "";
+    const terms = req.query.term || "";
+    const gen = req.query.gen || "";
+    const cmin = req.query.cmin || 0;
+    const cmax = req.query.cmax || 18;
+    const query = `${q}${q.trim().length > 0 ? "*" : ""}${subjects[0].length > 0 ? ` @subjectCode:{${subjects.join("|")}}` : ""}${terms[0].length > 0 ? ` @terms:{${terms.join("|")}}` : ""}${gen[0].length > 0 ? gen.map((g) => ` @gened:{${g}}`) : ""} (@creditMin:[${cmin}, ${cmax}] | @creditMax:[${cmin}, ${cmax}])`
+    const courses = await fetch("127.0.0.1:5000/query?q=" + query)
+      .then((response) => response.json())
     res.status(200).json({ courses });
   }
 }
