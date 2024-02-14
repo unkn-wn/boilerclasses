8834ca0e25fdbb10c1e30b0e90a054308813362c
sarthak <sarthak.robo@gmail.com>
2024-02-14 00:05:18 -0500

    add home pages and alt url

--- server/sitemap.py
+++ server/sitemap.py
@@ -1,8 +1,16 @@
 import json
 
 with open('../public/sitemap.xml', 'w') as xml_file:
-    xml_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    xml_file.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+  xml_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+  xml_file.write('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
+  xml_file.write('\t<url>\n')
+  xml_file.write(f'\t\t<loc>https://www.boilerclasses.com/</loc>\n')
+  xml_file.write(f'\t\t<priority>1</priority>\n')
+  xml_file.write('\t</url>\n')
+  xml_file.write('\t<url>\n')
+  xml_file.write(f'\t\t<loc>https://boilerclasses.com/</loc>\n')
+  xml_file.write(f'\t\t<priority>1</priority>\n')
+  xml_file.write('\t</url>\n')
 
 with open('classes_out.json', 'r') as file:
   data = json.load(file)
@@ -13,6 +21,10 @@ with open('classes_out.json', 'r') as file:
     with open('../public/sitemap.xml', 'a') as xml_file:
       xml_file.write('\t<url>\n')
       xml_file.write(f'\t\t<loc>https://www.boilerclasses.com/detail/{class_data["detailId"]}</loc>\n')
+      xml_file.write(f'\t\t<priority>{round(len(class_data["terms"])/mx_terms, 2)}</priority>\n')
+      xml_file.write('\t</url>\n')
+      xml_file.write('\t<url>\n')
+      xml_file.write(f'\t\t<loc>https://boilerclasses.com/detail/{class_data["detailId"]}</loc>\n')
       xml_file.write(f'\t\t<priority>{round(len(class_data["terms"])/mx_terms, 2)}</priority>\n')
       xml_file.write('\t</url>\n')
 
