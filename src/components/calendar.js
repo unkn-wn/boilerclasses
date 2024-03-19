a315b2f5c125c7f5001f97bf401c02b8d71b207f
sarthak <sarthak.robo@gmail.com>
2024-03-19 10:41:00 -0400

    add schedule semester

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -119,6 +119,7 @@ const Calendar = (props) => {
 
     return (
         <>
+            <div className="mb-2 ml-2 text-sm text-gray-500">Spring 2024 Schedule: </div>
             {/* Calendar View for Lecture Times */}
             <div className='grid grid-cols-1 md:grid-cols-5 w-full rounded-xl bg-zinc-900 p-2 md:p-4'>
                 <div className='md:border-r-2 md:pr-4 border-gray-500'>
