5e6cc030ed97074a8e51940a97d37b674d939484
sarthak <sarthak.robo@gmail.com>
2024-03-23 01:17:00 -0400

    fix sitemap

--- server/sitemap.py
+++ server/sitemap.py
@@ -19,10 +19,10 @@ with open('classes_out.json', 'r') as file:
     mx_terms = max(mx_terms, len(class_data['terms']))
   for class_data in data:
     with open('../public/sitemap.xml', 'a') as xml_file:
-      xml_file.write('\t<url>\n')
-      xml_file.write(f'\t\t<loc>https://www.boilerclasses.com/detail/{class_data["detailId"]}</loc>\n')
-      xml_file.write(f'\t\t<priority>{round(len(class_data["terms"])/mx_terms, 2)}</priority>\n')
-      xml_file.write('\t</url>\n')
+      # xml_file.write('\t<url>\n')
+      # xml_file.write(f'\t\t<loc>https://www.boilerclasses.com/detail/{class_data["detailId"]}</loc>\n')
+      # xml_file.write(f'\t\t<priority>{round(len(class_data["terms"])/mx_terms, 2)}</priority>\n')
+      # xml_file.write('\t</url>\n')
       xml_file.write('\t<url>\n')
       xml_file.write(f'\t\t<loc>https://boilerclasses.com/detail/{class_data["detailId"]}</loc>\n')
       xml_file.write(f'\t\t<priority>{round(len(class_data["terms"])/mx_terms, 2)}</priority>\n')
