106b0a22ececcf369d0438e561959a0449cd0d22
Leon <leon.yee000@gmail.com>
2024-05-15 23:14:19 -0700

    added footer to initial page
    Finished rest of #97 , cleaned up some UI elements

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,11 @@ import {
 } from '@chakra-ui/react'
 
 
-const Footer = () => {
-    const handleGoToTop = ()=> window.scrollTo({top:"0px",behavior:"smooth"})
+const Footer = ({ hideButton }) => {
+    const handleGoToTop = () => window.scrollTo({ top: "0px", behavior: "smooth" })
     return (
         <div className='grid justify-center py-5 relative'>
-            <h1 className='text-gray-400 text-sm text-center break-words'>
+            <h1 className='mx-2 text-gray-400 text-sm text-center break-words'>
                 <span className='flex items-center justify-center'>
                     <a href='https://github.com/unkn-wn/boilerclasses' target="_black" rel="noopener noreferrer" className='text-gray-400 flex items-center gap-1 underline decoration-dashed decoration-1 underline-offset-2'>
                         <Image src="https://icones.pro/wp-content/uploads/2021/06/icone-github-grise.png" alt="" boxSize={4} />
@@ -19,13 +19,15 @@ const Footer = () => {
                     </a>
                     {/* <p>&nbsp;(last deployed 1/2/24)</p> */}
                 </span>
-                is an unofficial catalog for Purdue courses made by Purdue students, <br />
+                is an unofficial catalog for Purdue courses made by Purdue students, <br className='hidden md:block' />
                 based in <strong>West Lafayette, Indiana</strong>. We'd love to hear your <a href="https://forms.gle/jtpLPbXm4X4RFoNh6" target="_black" rel="noopener noreferrer" className='underline decoration-dashed decoration-1 underline-offset-2'>feedback</a>!
                 {/* Inspired by <a href='https://classes.wtf/' target='_blank' rel='noopener noreferrer' className='underline decoration-dashed decoration-1 underline-offset-2'>classes.wtf</a><br /> */}
             </h1>
-            <Button position="absolute" right="0" top="-10px" borderRadius="50%" width={[8,12]} height={[10,12]} onClick={handleGoToTop}>
-                <ArrowUpIcon fontSize={[18,24]}/>
-            </Button>
+            {!hideButton && (
+                <Button position="absolute" right="0" top="-10px" borderRadius="50%" width={[8, 12]} height={[10, 12]} onClick={handleGoToTop}>
+                    <ArrowUpIcon fontSize={[18, 24]} />
+                </Button>
+            )}
         </div>
     );
 };
