e0ae9d1c3c0009aca17b7fa48077a3d6cb7c33fc
Leon <leon.yee000@gmail.com>
2024-07-24 19:55:40 -0700

    Remove unrelated files

--- server/script.sh
+++
@@ -1,4 +0,0 @@
-redis-server --daemonize yes --loadmodule /opt/redis-stack/lib/redisearch.so --loadmodule /opt/redis-stack/lib/rejson.so
-python3 push.py
-cd ..
-npm run start
--- src/pages/api/get.js
+++
@@ -1,9 +0,0 @@
-import { getCourse } from "../../lib/redis"
-
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    const detailId = req.query.detailId;
-    const course = await getCourse(detailId);
-    res.status(200).json({ course });
-  }
-}
