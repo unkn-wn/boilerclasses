64c090027707bc42d1d09df791150e0b42e9dd2a
sarthak <sarthak.robo@gmail.com>
2024-11-29 03:08:48 -0800

    add middleware

---
+++ src/middleware.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+const subdomains = [
+  {
+    "subdomain": "schedule"
+  }
+]
+
+export const config = {
+  matcher: [
+    "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
+  ],
+};
+
+export default async function middleware(req) {
+  const url = new URL(req.url);
+  const hostname = req.headers.get("host") || "";
+
+  const allowedDomains = ["localhost:3000", "boilerclasses.com", "pr-138-unkn-wn-boilerclasses.fly.dev"];
+  // const allowedDomains = ["localhost:3000", "boilerclasses.com"];
+
+  const isAllowedDomain = allowedDomains.some(domain => hostname.includes(domain));
+
+  const subdomain = hostname.split(".")[0];
+
+  if (isAllowedDomain && !subdomains.some(d => d.subdomain === subdomain)) {
+    return NextResponse.next();
+  }
+
+  const subdomainData = subdomains.find(d => d.subdomain === subdomain);
+
+  if (subdomainData) {
+    console.log(url, subdomain);
+    return NextResponse.rewrite(new URL(`/${subdomain}${url.pathname}`, req.url));
+  }
+
+  return new Response(null, { status: 404 });
+}
