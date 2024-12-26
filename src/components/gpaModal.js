17a6ef276ddfb08d2773d2f197fb50ff40fcedf4
Soham Jog <jogsoham2003@gmail.com>
2024-12-26 23:47:44 +0530

    gpa modal

--- src/components/gpaModal.js
+++ src/components/gpaModal.js
@@ -23,10 +23,10 @@ const GpaModal = ({ course }) => {
 
 	return (
 		<div className='h-[32rem] overflow-y-auto flex flex-col'>
-			<h1 className='text-white text-2xl font-bold'>GPA Breakdown</h1>
-			<h3 className='text-white text-sm'>
+			<h1 className='text-primary text-2xl font-bold'>GPA Breakdown</h1>
+			<h3 className='text-primary text-sm'>
 				This graphic displays all the professors along with their GPA for each semester. Use the search bar to filter for a specific professor!<br />
-				GPA: <span className='bg-[#632230] px-2'>1.0</span> ― <span className='bg-[#ddaa33] px-2 text-black'>4.0</span>
+				GPA: <span className='bg-[#632230] px-2'>1.0</span> ― <span className='bg-[#ddaa33] px-2 text-opposite'>4.0</span>
 			</h3>
 			<SearchBar
 				placeholder="Filter instructors..."
@@ -36,7 +36,7 @@ const GpaModal = ({ course }) => {
 			<div className='mt-2'>
 				{filteredInstructors.map((instructor, index) => (
 					<div key={index} className='flex flex-col mt-5'>
-						<h2 className='text-white font-bold text-xl'>{instructor}</h2>
+						<h2 className='text-primary font-bold text-xl'>{instructor}</h2>
 						<div className='grid grid-flow-col auto-cols-fr justify-stretch'>
 							{Object.keys(gpa[instructor]).map((semester, index) => (
 								// console.log(`bg-[${gpa[instructor][semester].color}]`),
@@ -44,9 +44,9 @@ const GpaModal = ({ course }) => {
 									<div className='grid h-12 text-center' style={{ backgroundColor: `${gpa[instructor][semester].color}` }}>
 										<p className='text-white m-auto font-semibold'>{replaceZeroGpaWithDash(gpa[instructor][semester].gpa)}</p>
 									</div>
-									<h3 className='text-zinc-500 text-center text-sm px-1 hidden md:block'>{semester}</h3>
-									<h3 className='text-zinc-500 text-center text-[10px] px-1 block md:hidden'>{semester.split(" ")[0]}</h3>
-									<h3 className='text-zinc-500 text-center text-sm px-1 block md:hidden'>{" '" + semester.split(" ")[1].substring(2, 4)}</h3>
+									<h3 className='text-secondary text-center text-sm px-1 hidden md:block'>{semester}</h3>
+									<h3 className='text-secondary text-center text-[10px] px-1 block md:hidden'>{semester.split(" ")[0]}</h3>
+									<h3 className='text-secondary text-center text-sm px-1 block md:hidden'>{" '" + semester.split(" ")[1].substring(2, 4)}</h3>
 								</div>
 							))}
 						</div>
@@ -100,12 +100,12 @@ export const ScheduleGpaModal = ({ course }) => {
 					</div>
 					{Object.keys(gpa).map((instructor, index) => (
 						<div key={index} className='grid grid-flow-row py-1 gap-1'>
-							<h2 className='text-white text-xs'>{instructor}</h2>
+							<h2 className='text-primary text-xs'>{instructor}</h2>
 							<div className='w-full grid grid-flow-col auto-cols-fr justify-stretch gap-1'>
 								{Object.keys(gpa[instructor]).map((semester, index) => (
 									<div key={index} className='flex flex-col'>
 										<div className='grid py-1 text-center rounded-md' style={{ backgroundColor: `${gpa[instructor][semester].color}` }}>
-											<p className='text-white m-auto text-sm font-light'>{replaceZeroGpaWithDash(gpa[instructor][semester].gpa)}</p>
+											<p className='text-primary m-auto text-sm font-light'>{replaceZeroGpaWithDash(gpa[instructor][semester].gpa)}</p>
 										</div>
 									</div>
 								))}
@@ -115,7 +115,7 @@ export const ScheduleGpaModal = ({ course }) => {
 				</>
 			) : (
 				<div>
-					<p className='text-white text-sm font-light text-left'>No data for current instructors.</p>
+					<p className='text-primary text-sm font-light text-left'>No data for current instructors.</p>
 				</div>
 			)}
 		</div>
