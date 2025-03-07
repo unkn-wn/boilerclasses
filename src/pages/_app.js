c5ea1160866b4801817ad6c16a2297e3bccd971f
Leon <43097991+unkn-wn@users.noreply.github.com>
2025-03-07 16:29:12 -0500

    Add tag manager

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -45,6 +45,16 @@ export default function App({ Component, pageProps }) {
           `
           }}
         />
+        <Script id="google-tab-manager">
+          {`
+            (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P8N23MSR');
+          `}
+        </Script>
+
         <Head>
           <title>BoilerClasses</title>
         </Head>
